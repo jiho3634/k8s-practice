287192e4eace2f78183d2b8e6165a44efbf1239e
Jiho Kim <jiho3634@gmail.com>
2024-08-30 17:46:18 +0900

    fix

--- ordersystem/src/main/java/com/beyond/ordersystem/common/configs/CorsConfig.java
+++ ordersystem/src/main/java/com/beyond/ordersystem/common/configs/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://www.jiho-test.store")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
