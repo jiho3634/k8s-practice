12f9495cb3a19efe4b962254986ee5dc9499ea5a
Jiho Kim <jiho3634@gmail.com>
2024-09-03 10:24:56 +0900

    msa

---
+++ msa/product/src/main/java/com/beyond/ordersystem/product/controller/ProductController.java
@@ -0,0 +1,68 @@
+package com.beyond.ordersystem.product.controller;
+
+import com.beyond.ordersystem.common.dto.CommonErrorDto;
+import com.beyond.ordersystem.common.dto.CommonResDto;
+import com.beyond.ordersystem.product.domain.Product;
+import com.beyond.ordersystem.product.dto.ProductSaveReqDto;
+import com.beyond.ordersystem.product.dto.ProductSearchDto;
+import com.beyond.ordersystem.product.dto.ProductUpdateStockDto;
+import com.beyond.ordersystem.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+//  해당 어노테이션 사용시 아래의 Bean 은 실시간 config 변경 사항의 대상이 된다.
+@RestController
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/product/create")
+    public ResponseEntity<?> productCreate(ProductSaveReqDto dto) {
+        try {
+            Product product = productService.productAwsCreate(dto);
+            CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "Product is successfully created", product.getId());
+            return new ResponseEntity<>(commonResDto, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(new CommonErrorDto(HttpStatus.BAD_REQUEST.value(), "BAD REQUEST"), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/product/awscreate")
+    public ResponseEntity<?> productAwsCreate(ProductSaveReqDto dto) {
+        try {
+            Product product = productService.productAwsCreate(dto);
+            CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "Product is successfully created", product.getId());
+            return new ResponseEntity<>(commonResDto, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(new CommonErrorDto(HttpStatus.BAD_REQUEST.value(), "BAD REQUEST"), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/product/list")
+    public ResponseEntity<?> productList(ProductSearchDto searchDto, Pageable pageable) {
+        return new ResponseEntity<>(new CommonResDto(HttpStatus.OK, "정상조회완료", productService.productList(searchDto, pageable)), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<?> productDetail(@PathVariable Long id) {
+        return new ResponseEntity<>(new CommonResDto(HttpStatus.OK, "정상조회완료", productService.productDetail(id)), HttpStatus.OK);
+    }
+
+    @PutMapping("/product/updatestock")
+    public ResponseEntity<?> productStockUpdate(@RequestBody ProductUpdateStockDto dto) {
+        Product product = productService.productUpdateStock(dto);
+        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "update is successfull", product.getId());
+        return new ResponseEntity<>(commonResDto, HttpStatus.OK);
+    }
+}
